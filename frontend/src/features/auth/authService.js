416d6df639f62b8c9ee4136e8735e07570cc0b2c
rob <robert_regan86@yahoo.com>
2022-09-22 17:13:24 -0400

    register, login, logout working ver nice

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,8 +15,26 @@ const register = async (userData) => {
     return response.data
 }
 
+// login user 
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.data){
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+// logout function.. could use server and set an http-only cookie but this is a simple way for now.
+const logout = () => {
+    localStorage.removeItem('user')
+}
+
 const authService = {
-    register
+    register,
+    logout, 
+    login
 }
 
 export default authService
